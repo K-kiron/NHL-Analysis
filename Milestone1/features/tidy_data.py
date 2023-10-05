e79ec6daab5c7ddb6cfc0f41d3fe52aa7c96bcd6
Wenhao XU <70655683+K-kiron@users.noreply.github.com>
2023-10-05 00:58:56 -0400

    debug & sv q2 finish

--- Milestone1/features/tidy_data.py
+++ Milestone1/features/tidy_data.py
@@ -10,6 +10,8 @@ def tidy_data(path, year, season, game_id):
     shot_data_temp = []
     
     playData = game_data['liveData']['plays']['allPlays']
+    homeTeam = game_data['gameData']['teams']['home']['triCode']
+    awayTeam = game_data['gameData']['teams']['away']['triCode']
     for i in range(len(playData)):
         eventData = playData[i]
         if eventData['result']['event'] == 'Shot' or eventData['result']['event'] == 'Goal':
@@ -17,28 +19,54 @@ def tidy_data(path, year, season, game_id):
             periodType = eventData['about']['periodType']
             period = eventData['about']['period']
             periodTime = eventData['about']['periodTime']
-            team = eventData['team']['name']
+            team = eventData['team']['triCode']
             eventType = eventData['result']['event']
-            coordinates = eventData['coordinates']
+            try:
+                x_coordinate = eventData['coordinates']['x']
+                y_coordinate = eventData['coordinates']['y']
+            except KeyError:
+                x_coordinate = None
+                y_coordinate = None
             shooter = eventData['players'][0]['player']['fullName']
             goalie = eventData['players'][-1]['player']['fullName']
-            shotType = eventData['result']['secondaryType']
+            try:
+                shotType = eventData['result']['secondaryType']
+            except KeyError:
+                shotType = None
             if eventType == 'Goal':
-                emptyNet = eventData['result']['emptyNet']
+                try:
+                    emptyNet = eventData['result']['emptyNet']
+                except KeyError:
+                    emptyNet = None
                 strength = eventData['result']['strength']['code']
             else:
                 emptyNet = None
                 strength = None
+                
+            if team == homeTeam:
+                if period%2 != 0:
+                    goalLocation = 'Left'
+                else:
+                    goalLocation = 'Right'
+                    
+            elif team == awayTeam:
+                if period%2 != 0:
+                    goalLocation = 'Right'
+                else:
+                    goalLocation = 'Left'
 
-            # Store the data in a dictionary
             shot_data = {
                 'game_id': game_id,
+                'homeTeam': homeTeam,
+                'awayTeam': awayTeam,
                 'periodType': periodType,
                 'period': period,
                 'periodTime': periodTime,
                 'team': team,
                 'eventType': eventType,
-                'coordinates': coordinates,
+                'x_coordinate': x_coordinate,
+                'y_coordinate': y_coordinate,
+                'goal_location': goalLocation,
                 'shooter': shooter,
                 'goalie': goalie,
                 'shotType': shotType,
@@ -51,4 +79,4 @@ def tidy_data(path, year, season, game_id):
     return pd.DataFrame(shot_data_temp)
 
 #test case below
-#print(tidy_data([path], 2017, 'regular', 2017020001))
+#print(tidy_data(2017, 'regular', 2017020001))
