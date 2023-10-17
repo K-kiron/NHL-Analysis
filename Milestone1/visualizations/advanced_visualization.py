df84177c274bedccd18062a98811ba5b85733f01
yaldakasravi <yaldakasravi@yajoo.com>
2023-10-16 21:23:52 -0400

    final commit

--- Milestone1/visualizations/advanced_visualization.py
+++ Milestone1/visualizations/advanced_visualization.py
@@ -11,12 +11,15 @@ import plotly.express as px
 import plotly.graph_objects as go
 from PIL import Image
 import plotly.express as px
+import plotly.offline as pyo
+import pdb
 #from dash import Dash, dcc, html
-sys.path.append("IFT6758B-Project-B10/Milestone1/features")
+
+sys.path.append("/Users/yaldakasravi/Desktop/IFT6758B-Project-B10/Milestone1/features")
 
 from tidy_data import tidy_data
 def load_tidy_data(year, season):
-    path = f"IFT6758B-Project-B10/Milestone1/data/IFT6758_Data"
+    path = f"/Users/yaldakasravi/Desktop/IFT6758B-Project-B10/Milestone1/data/IFT6758_Data"
 
     # Define game_type based on season
     game_type = '02' if season.lower() == 'regular' else '03'
@@ -48,7 +51,7 @@ def load_tidy_data(year, season):
 
 # Function to load the rink image
 def load_rink_image(image_path):
-    return Image.open("IFT6758B-Project-B10/Milestone1/visualizations/nhl_rink.png")
+    return Image.open("/Users/yaldakasravi/IFT6758B-Project-B10/Milestone1/visualizations/nhl_rink.png")
 
 # Define offensive zone coordinates (adjust these based on your specific rink dimensions)
 offensive_zone_x_min = 0
@@ -56,17 +59,43 @@ offensive_zone_x_max = 100
 offensive_zone_y_min = -42.5
 offensive_zone_y_max = 42.5
 
+def get_season_score(data):
 
+    X = []
+    Y = []
+    X_orig = []
+    Y_orig = []
+    for _, shot in data.iterrows():
+        x = shot['x_coordinate']
+        y = shot['y_coordinate']
+
+        X_orig.append(x)
+        Y_orig.append(y)
+        if shot['goal_location'] == "Left":
+            x = -x
+            y = -y
+
+        if (offensive_zone_x_min <= x <= offensive_zone_x_max) and (offensive_zone_y_min <= y <= offensive_zone_y_max):
+            # Calculate adjusted coordinates with offsets
+            x_adjusted = x 
+            y_adjusted = y 
+
+            X.append(x_adjusted)
+            Y.append(y_adjusted)
+
+    return list(zip(X,Y))
+
+        # Add a scatter point to the figure
 
 # Function to create a shot map on the rink image for a selected team
-def create_shot_map_for_team(data, rink_image, selected_team):
-    fig = go.Figure()
+def create_shot_map_for_team(data, rink_image, selected_team,team_average,year):
 
 
     # Define scale factors based on the rink image dimensions and scaling
     scale_factor_x = 1 #38.81 / 6100  # Assuming rink width is 200 units
     scale_factor_y = 1 #16.47 / 3000  # Assuming rink height is 85 units
 
+    unique_teams = data['team'].unique()
     team_data = data[data['team'] == selected_team]
     X = []
     Y = []
@@ -94,48 +123,92 @@ def create_shot_map_for_team(data, rink_image, selected_team):
 
         # Add a scatter point to the figure
        #fig.add_trace(go.Scatter(x=[x_adjusted], y=[y_adjusted], mode='markers', marker=dict(color=color, size=10)))
-    print("max in X {}".format(max(X_orig)))
-    print("max in Y {}".format(max(Y_orig)))
+    Team_data = np.array(list(zip(X,Y)))
+    season_data = np.array(team_average)
+    x_bins = np.linspace(-42.5,42.5,400)
+    y_bins = np.linspace(-100,100,400)
 
-    print("min in X {}".format(min(X_orig)))
-    print("min in Y {}".format(min(Y_orig)))
+    hist_team,x_e,y_e = np.histogram2d(Team_data[:,0],Team_data[:,1],bins=[x_bins,y_bins]) 
+    hist_avg,x_ea,y_ea = np.histogram2d(season_data[:,0],season_data[:,1],bins=[x_bins,y_bins]) 
 
+    diff_hist = (((hist_team  )- hist_avg) )/ 60*(len(unique_teams) * len(unique_teams))
+    #diff_hist_y = hist_team[1] - hist_avg[1]
+    x_values = (x_e[1:] + x_e[:-1]) / 2
+    y_values = (y_e[1:] + y_e[:-1]) / 2 
+
+    xx,yy = np.meshgrid(x_values,y_values)
+    xx = xx.reshape(-1)
+    yy = yy.reshape(-1)
+
+    #hist_1d = diff_hist.reshape(-1)
+    hist_1d = diff_hist.reshape(-1)
+
+# Create a dataframe from the values
+    data_df = pd.DataFrame({'x': np.repeat(x_values, len(y_values)),
+                        'y': np.tile(y_values, len(x_values)),
+                        'hist': hist_1d})
+    #pdb.set_trace()
+    
     sns.set_style("white")
     #sns.cubehelix_palette(as_cmap=True)
-    sns.kdeplot(x=X, y=Y,cmap='icefire', shade=True, bw_adjust=.5,cbar=True,alpha=0.6)
-    plt.ylim(-42.5,42.5)
-    plt.xlim(-100,100)
-    plt.imshow(rink_image, extent=[-100,100,-42.5, 42.5], aspect='auto')
+    #sns.kdeplot(x=X,y=Y,cmap='icefire', shade=True, bw_adjust=.5,cbar=True,alpha=0.6)
+    #plt.ylim(-42.5,42.5)
+    #plt.xlim(-100,100)
+    #plt.imshow(rink_image, extent=[-100,100,-42.5, 42.5], aspect='auto')
 
-    plt.show()
+    #plt.show()
     #plt.scatter(x=X,y=Y)
 
     # Add surface trace
     #fig.add_trace(go.Surface(z=df.values.tolist(), colorscale="Viridis"))
-    # Customize the figure layout
-    fig.update_layout(
-        width=800,
-        height=900,
-        autosize=False,
-        margin=dict(t=0, b=0, l=0, r=0),
-        template="plotly_white",
-        title=f"Shot Map for {selected_team}",
-        xaxis_title="X Coordinate",
-        yaxis_title="Y Coordinate",
-    )
+    # Customize the figure layouAt
+    #Z = [ 1 for _ in X ]
+    #df = pd.DataFrame(list(zip(X,Y,Z)))
+    fig = go.Figure(px.density_heatmap(
+     x=yy,  # x-values from your histogram
+     y=xx,  # y-values from your histogram
+     z=hist_1d  # Transpose the histogram to match x, y shape
+     ))
 
-    # Display the rink image as a background image
-    fig.add_layout_image(
-        source=rink_image,
-        xref="x",
-        yref="y",
-        x=0,
-        y=85,  # Adjust this value based on the image positioning
-        sizex=200,
-        sizey=85,
-        opacity=1,
-        layer="below"
-    )
+# Set the layout to display the heatmap properly
+    #fig.update_layout(
+    #     mapbox=dict(
+    #         center=dict(lat=np.mean(y_values), lon=np.mean(x_values)),
+    #         style="carto-positron",
+    #         zoom=10,
+    #     )
+    # )
+
+# Show the plot
+    fig.show()
+
+    
+    fig = px.density_heatmap(data_df)
+    #fig = px.density_mapbox(df,lat=0,lon=1,z=2  )
+    #    height=900,
+    #    autosize=False,
+    #    margin=dict(t=0, b=0, l=0, r=0),
+    #    template="plotly_white",
+    #    title=f"Shot Map for {selected_team}",
+    #    xaxis_title="X Coordinate",
+    #    yaxis_title="Y Coordinate",
+    #)
+
+    ## Display the rink image as a background image
+    #fig.add_layout_image(
+    #    source=rink_image,
+    #    xref="x",
+    #    yref="y",
+    #    x=0,
+    #    y=85,  # Adjust this value based on the image positioning
+    #    sizex=200,
+    #    sizey=85,
+    #    opacity=1,
+    #    layer="below"
+    #)
+
+    #fig.write_html('/Users/yaldakasravi/IFT6758B-Project-B10/Milestone1/blog/_includes/plotly_shotmap.html')
+    fig.write_html('../blog/_includes/plotly_shotmap_{}_{}.html'.format(selected_team,year))
 
     return fig
 
@@ -148,30 +221,37 @@ if __name__ == "__main__":
     game_type = {'REGULAR': '02', 'PLAYOFFS': '03'}
     game_type = game_type.get(season.upper())  # Convert to uppercase for case-insensitive matching
 
+    data = load_tidy_data(year, season)
+    unique_teams = data['team'].unique()
+    season_avg = get_season_score(data)
+    team_name_dict = {team: team for team in unique_teams}
+
     if season is None:
         print("Invalid season input. Please enter 'REGULAR' or 'PLAYOFFS'.")
     else:
         # Get unique team names for the specified year and season
-        data = load_tidy_data(year, season)
-        unique_teams = data['team'].unique()
 
         # Create a dictionary for team names
-        team_name_dict = {team: team for team in unique_teams}
 
         # Prompt the user to select a team from the dictionary
-        selected_team = input(f"Select a team from the following: {', '.join(unique_teams)}: ")
+        while(1):	
+        	selected_team = input(f"Select a team from the following: {', '.join(unique_teams)}: ")
 
-        # Load the rink image
-        rink_image_path = "IFT6758B-Project-B10/Milestone1/visualizations/nhl_rink.png"
-        rink_image = load_rink_image(rink_image_path)
+        	# Load the rink image
+        	rink_image_path = "/Users/yaldakasravi/IFT6758B-Project-B10/Milestone1/visualizations/nhl_rink.png"
+        	rink_image = load_rink_image(rink_image_path)
 
-        # Create the shot map for the selected team
-        fig = create_shot_map_for_team(data, rink_image, selected_team)
-        # Display the interactive plot
-        fig.show()
-        """app = Dash()
-        app.layout = html.Div([
-            dcc.Graph(figure=fig)
-            app.run_server(debug=True, use_reloader=False)
-        ])"""
-
+        	# Create the shot map for the selected team
+        	print("in create shot")
+        	fig = create_shot_map_for_team(data, rink_image, selected_team,season_avg,year)
+        	# Display the interactive plot
+        	#fig.show()
+			
+        	#fig.write_html('/Users/yaldakasravi/IFT6758B-Project-B10/Milestone1/blog/_includes/plotly_shotmap.html')
+        	#fig.write_html('../blog/plotly_shotmap.html')
+        	print("done writing the plot")
+        	"""app = Dash()
+        	app.layout = html.Div([
+        	    dcc.Graph(figure=fig)
+        	    app.run_server(debug=True, use_reloader=False)
+        	])"""
