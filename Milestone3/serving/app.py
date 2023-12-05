52dd2b06f761fd2bd52ba9dbb9ed12f4a019e88a
tristmartin <123830223+tristmartin@users.noreply.github.com>
2023-12-05 10:18:46 -0500

    Fixed API key to generic

--- Milestone3/serving/app.py
+++ Milestone3/serving/app.py
@@ -66,8 +66,7 @@ def download_registry_model():
         version = json_data.get('version')      # model version (ex: "1.0.1")
 
         # Specify COMET_API_KEY below
-        #api = API(api_key="example_key_1234")
-        api = API(api_key="M0ld212AYoT5RG6UcLL807o5T")
+        api = API(api_key="example_key_1234")
 
         # TODO: check to see if the model you are querying for is already downloaded
         model_path = f"{model_name}.pkl"
