594f697f614ae64ad09a5428f1ddf13c112bfdc1
Uday Karan Kapur <udaykarankapur@Udays-Air.ht.home>
2023-12-22 16:56:58 -0500

    fix build.sh and run.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build --no-cache -t serving_image -f Dockerfile.serving .
-docker build --no-cache -t frontend_image -f Dockerfile.frontend .
+docker build --no-cache -t frontend_image -f Dockerfile.streamlit .
