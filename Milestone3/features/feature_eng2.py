b822d1c012ee103b0f48babdc8b21441513a80a1
Wenhao XU <70655683+K-kiron@users.noreply.github.com>
2023-12-22 09:43:34 -0500

    standardize in milestone 3 format
    replace API key with placeholder & modify PROJECT_PATH

--- Milestone3/features/feature_eng2.py
+++ Milestone3/features/feature_eng2.py
@@ -7,8 +7,7 @@ import re
 import sys
 
 DATA_PATH = '../../IFT6758_Data/'
-PROJECT_PATH = '../../Milestone2/'
-
+PROJECT_PATH = '../../Milestone3/'
 
 sys.path.append(PROJECT_PATH)
 from features.tidy_data import tidy_data
